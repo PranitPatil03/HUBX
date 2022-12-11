06c15245768eb1c4f121499845276ed3d7c26d07
PranitPatil03 <patilpranit3112@gmail.com>
2022-12-12 01:04:31 +0530

    Navigration Bar Compeleted

--- dashboard/src/components/Navbar.js
+++ dashboard/src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import "./style.css"
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <>
+      <section id="topnavbar">
+        <div className="navigration">
+          <div className="navbar">
+            <div className="logo">
+              <h2>Dashboard</h2>
+            </div>
+            <div className="navbaritems">
+              <ul>
+                <li><img src="" alt="" />Create Course</li>
+                <li><img src="" alt="" />Preview home page</li>
+                <li><img src="" alt="" />Preview after login page</li>
+                <li><img src="" alt="" />View welome screen</li>
+              </ul>
+            </div>
+            <div className="help">
+              <img src="" alt="" />
+              <p>Help</p>
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </>
   )
 }
 
