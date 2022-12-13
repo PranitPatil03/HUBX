484cb18b4491f2051952e45f3840a2dcbbac0a5d
PranitPatil03 <patilpranit3112@gmail.com>
2022-12-14 03:45:11 +0530

    Bottom Section Completed

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Graph from './components/Graph';
 import Navbar from './components/Navbar';
+import Newuser from './components/Newuser';
 
 function App() {
   return (
     <>
       <Navbar />
       <Graph />
+      <Newuser/>
     </>
   );
 }
--- dashboard/src/components/Newuser.js
+++ dashboard/src/components/Newuser.js
@@ -2,7 +2,119 @@ import React from 'react'
 
 function Newuser() {
   return (
-    <div>Newuser</div>
+    <>
+      <div className="maincontainer">
+        <div className="users1">
+          <div className="user1">
+            <div>
+              New users <button>sell all</button>
+            </div>
+            {/* <img className="userimg" src="" alt="" /> */}
+            <div>
+              <p className="userpara1">Hubx</p>
+              <br />
+              <p className="userpara">24 minutes</p>
+            </div>
+          </div>
+        </div>
+        <div className="blog">
+          <div className="blog1">
+            How to sell course blog<button>sell all</button>
+          </div>
+          <div className="head">
+            <div className="content">
+              Blended Learning : what It Is, <br />Why It  Matters & How to Apply
+            </div>
+            <div className="day">
+              7 day ago
+            </div>
+          </div>
+          <div className="head">
+            <div className="content">
+              Blended Learning : what It Is, <br />Why It  Matters & How to Apply
+            </div>
+            <div className="day">
+              7 day ago
+            </div>
+          </div>
+          <div className="head">
+            <div className="content">
+              Blended Learning : what It Is, <br />Why It  Matters & How to Apply
+            </div>
+            <div className="day">
+              7 day ago
+            </div>
+          </div>
+          <div className="head">
+            <div className="content">
+              Blended Learning : what It Is, <br />Why It  Matters & How to Apply
+            </div>
+            <div className="day">
+              7 day ago
+            </div>
+          </div>
+          <div className="head">
+            <div className="content">
+              Blended Learning : what It Is, <br />Why It  Matters & How to Apply
+            </div>
+            <div className="day">
+              7 day ago
+            </div>
+          </div>
+        </div>
+        <div className="eventlog">
+        <div className="users1">
+          <div className="user1">
+            <div>
+              Event Log<button>sell all</button>
+            </div>
+         
+            <div className="head">
+            <div className="content">
+              <span className="small">hubx</span><br />
+              logged in <br />
+              more info
+            </div>
+            <div className="day extra">
+              7 day ago
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+        <div className="trailperiod">
+        <div className="users1">
+          <div className="user1">
+            <div>
+            Trail Period 
+            <br />
+            <div className="daysleft">
+               
+              <div> <span>30</span><br />DAYS lEFT</div>
+            </div>
+            </div>
+
+          </div>
+        </div>
+        <div className="users1">
+          <div className="user1">
+            <div>
+              New users(1) <button>sell all</button>
+            </div>
+            {/* <img className="userimg" src="" alt="" /> */}
+            <div className="head">
+            <div className="content">
+              Hubx
+            </div>
+            <div className="day extra1">
+              7 day ago
+            </div>
+          </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </>
   )
 }
 
